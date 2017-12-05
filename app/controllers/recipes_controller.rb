5bec9ee428c419136e1d4609ec610ee5b246f9af
Nathan Dunn <nathan.odin@gmail.com>
2017-12-05 14:34:25 -0500

    Recipe Controller - Auth before Delete and Update
    -(refactor) added delete and update to before_action for
                authenticate_user

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,5 @@
 class RecipesController < ApplicationController
-  before_action :authenticate_user!, only: [:create]
+  before_action :authenticate_user!, only: [:create, :update, :destroy]
   before_action :set_recipe, only: [:show, :update, :destroy]
 
   # GET /recipes
