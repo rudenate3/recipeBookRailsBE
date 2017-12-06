37e4b22feb5b803f4a10dd3a19eb585c31446167
Nathan Dunn <nathan.odin@gmail.com>
2017-12-06 18:26:06 -0500

    Ingredients / Curl
    -(feature) Scaffolded Ingredients table and routes
    -(feature) Create basic curl scripts for index and show

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205154352) do
+ActiveRecord::Schema.define(version: 20171205231803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name" # TODO this should be consistent with out models
+    t.string "description"
+    t.string "quantity_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.bigint "user_id"
