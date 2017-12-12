360baabb57ce2b1f98c3f781ab5714da0b3fde6d
Nathan Dunn <nathan.odin@gmail.com>
2017-12-12 17:48:47 -0500

    Seeds
    -(feature) Created basic seeds for recipes and ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+#   --- Need to create user first
+Recipe.create(user_id: 1, title: 'Burger', description: 'A Cheeseburger')
+Recipe.create(user_id: 1, title: 'Pasta', description: 'Some Pasta')
+Recipe.create(user_id: 1, title: 'French Fries', description: 'Potatoes')
+
+Ingredient.create(title: 'Bun', description: 'A sesame seed bun', quantity_type: 'bun')
+Ingredient.create(title: 'Ground Beef', description: 'Grade A Beef', quantity_type: 'pound')
+Ingredient.create(title: 'Cheese', description: 'Pepper Jack', quantity_type: 'slice')
+Ingredient.create(title: 'Pasta', description: 'Pasta', quantity_type: 'oz')
+Ingredient.create(title: 'Marinara Sauce', description: 'Marinara', quantity_type: 'oz')
+Ingredient.create(title: 'Potatoes', description: 'From Idaho', quantity_type: 'potatoes')
